4b89b8f3e701852a3b5ac4213d66393acbdccdf3
Andy Jung <wontaethedev@gmail.com>
2024-12-03 02:40:14 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+
 import spashIcon from "@/assets/splash-icon.png";
 
 function App() {
-  window.location.href = "playfit-indivisualiv://";
+  useEffect(() => {
+    const redirectToApp = () => {
+      const baseURL = "playfit-indivisualiv://";
+      const queryParams = new URLSearchParams({
+        token: "12345",
+      });
+
+      // Redirect to custom URL scheme with query params
+      window.location.href = `${baseURL}?${queryParams.toString()}`;
+    };
+
+    redirectToApp();
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
